04f6d21518f859caedb6ee140e6932485053d641
Disyam Adityana <disyam.adityana@gmail.com>
2023-05-05 10:12:56 +0700

    feat(secure): vault init

--- app.ts
+++ app.ts
@@ -25,52 +25,81 @@ const protections: Array<{ id: string; data: [string] }> = [
     data: ["123-456-7890"],
   },
 ];
+const vaults: Array<{ id: string; data: [string] }> = [
+  {
+    id: "b64e2268-fca6-4605-8b5a-307a315b266d",
+    data: ["1945-08-17"],
+  },
+];
+const id = Math.floor(Date.now() / 1000);
 
 async function store() {
   const {
-    data: { credential },
+    data: { credential: protectionCredential },
   } = await axios.post(`${backendUrl}/api/secure/protection/init`, {
     operation: "WRITE",
     protection_ids: protections.map((protection) => protection.id),
     ttl: 1,
   });
-  const { tokens } = await client.secureProtectionSend(
-    credential,
+  const { tokens: protectionTokens } = await client.secureProtectionSend(
+    protectionCredential,
     protections.map((protection) => protection.data)
+  );
+  const {
+    data: { credential: vaultCredential },
+  } = await axios.post(`${backendUrl}/api/secure/vault/init`, {
+    operation: "WRITE",
+    vault_ids: vaults.map((vault) => vault.id),
+    ttl: 1,
+  });
+  const { tokens: vaultTokens } = await client.secureVaultSend(
+    vaultCredential,
+    vaults.map((vault) => vault.data)
   );
   const query = gql`
     mutation store_user_secure($data: UserStoreInput!, $credential: String!) {
-      store_user_secure(data: $data, credential: $credential) {
-        id
-      }
+      store_user_secure(data: $data, credential: $credential)
     }
   `;
   const variables = {
     data: {
-      id: 100,
+      id,
       name: "Disyam Adityana",
-      email: tokens[0][0],
-      country: tokens[1][0],
-      credit_card: tokens[2][0],
-      phone: tokens[3][0],
+      email: protectionTokens[0][0],
+      country: protectionTokens[1][0],
+      credit_card: protectionTokens[2][0],
+      phone: protectionTokens[3][0],
+      birthdate: vaultTokens[0][0],
     },
-    credential,
+    credential: protectionCredential,
   };
-  console.log("store input", variables.data);
   await request(`${backendUrl}/graphql`, query, variables);
+  console.log("update OK");
 }
 
 async function update() {
   const {
-    data: { credential },
+    data: { credential: protectionCredential },
   } = await axios.post(`${backendUrl}/api/secure/protection/init`, {
     operation: "WRITE",
     protection_ids: [protections[0].id],
     ttl: 1,
   });
-  const { tokens } = await client.secureProtectionSend(credential, [
-    ["disyam.adityana@gmail.com"],
-  ]);
+  const { tokens: protectionTokens } = await client.secureProtectionSend(
+    protectionCredential,
+    [["disyam.adityana@gmail.com"]]
+  );
+  const {
+    data: { credential: vaultCredential },
+  } = await axios.post(`${backendUrl}/api/secure/vault/init`, {
+    operation: "WRITE",
+    vault_ids: [vaults[0].id],
+    ttl: 1,
+  });
+  const { tokens: vaultTokens } = await client.secureVaultSend(
+    vaultCredential,
+    [["2023-05-02"]]
+  );
   const query = gql`
     mutation update_user_secure(
       $id: Int!
@@ -81,39 +110,75 @@ async function update() {
     }
   `;
   const variables = {
-    id: 100,
-    data: { email: tokens[0][0] },
-    credential,
+    id,
+    data: {
+      email: protectionTokens[0][0],
+      birthdate: vaultTokens[0][0],
+    },
+    credential: protectionCredential,
   };
-  console.log("update input", variables.data);
   await request(`${backendUrl}/graphql`, query, variables);
+  console.log("store OK");
 }
 
 async function get() {
   const query = gql`
-    query get_user($id: Int!) {
-      get_user(id: $id) {
+    query get_user_secure($id: Int!) {
+      get_user_secure(id: $id) {
         id
         email
         country
         credit_card
         phone
+        birthdate
       }
     }
   `;
-  const variables = {
-    id: 100,
-  };
-  const { get_user: data } = await request(
+  const variables = { id };
+  const { get_user_secure: data } = await request(
     `${backendUrl}/graphql`,
     query,
     variables
   );
-  console.log("get data", data);
+  const {
+    data: { credential: protectionCredential },
+  } = await axios.post(`${backendUrl}/api/secure/protection/init`, {
+    operation: "READ",
+    protection_ids: protections.map((protection) => protection.id),
+    ttl: 1,
+  });
+  const protectionTokens: string[][] = [
+    [data.email],
+    [data.country],
+    [data.credit_card],
+    [data.phone],
+  ];
+  const { values: protectionValues } = await client.secureProtectionReceive(
+    protectionCredential,
+    protectionTokens
+  );
+  console.log("email:", protectionValues[0][0]);
+  console.log("country:", protectionValues[1][0]);
+  console.log("credit_card:", protectionValues[2][0]);
+  console.log("phone:", protectionValues[3][0]);
+  const {
+    data: { credential: vaultCredential },
+  } = await axios.post(`${backendUrl}/api/secure/vault/init`, {
+    operation: "READ",
+    vault_ids: vaults.map((vault) => vault.id),
+    ttl: 1,
+  });
+  const vaultTokens: string[][] = [[data.birthdate]];
+  const { values: vaultValues } = await client.secureVaultReceive(
+    vaultCredential,
+    vaultTokens
+  );
+  console.log("birthdate:", vaultValues[0][0]);
 }
 
 (async () => {
   await store();
+  await get();
   await update();
   await get();
 })();
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosInstance, isAxiosError } from "axios";
-import semver from "semver";
 import nacl from "tweetnacl";
-import { encodeBase64, decodeBase64, decodeUTF8 } from "tweetnacl-util";
-
-const expectedKastelaVersion = "v0.2";
+import {
+  encodeBase64,
+  decodeBase64,
+  decodeUTF8,
+  encodeUTF8,
+} from "tweetnacl-util";
 
 /**
  * @class
@@ -31,24 +33,12 @@ export class Client {
 
   async #request(method: string, url: URL, body?: any) {
     try {
-      const { data, headers } = await this.#axiosInstance.request({
+      const { data } = await this.#axiosInstance.request({
         url: url.toString(),
         method,
         data: body,
       });
-      const actualKastelaVersion = headers["x-kastela-version"]!;
-      if (
-        semver.satisfies(
-          actualKastelaVersion,
-          `${expectedKastelaVersion} || v0.0.0`
-        )
-      ) {
-        return data;
-      } else {
-        throw new Error(
-          `kastela server version mismatch, expected: ${expectedKastelaVersion}.x, actual: ${actualKastelaVersion}`
-        );
-      }
+      return data;
     } catch (error: any) {
       const data = error?.response?.data;
       if (data) {
@@ -66,7 +56,7 @@ export class Client {
 
   /** Send encrypted protection data to server.
    * @param {string} credential secure protection credential
-   * @param {any[][]} data protection data to be sent to the server. the sequence of data match the protection_id sequence on init
+   * @param {any[][]} values protection data to be sent to the server. the sequence of data match the protection_id sequence on init
    * @return {Promise<{tokens: string[][]}>} token that will be stored in the database. the tokens sequence corresponds to the data sequence
    * @example
    * 	// send "123","456","789" to server
@@ -74,7 +64,7 @@ export class Client {
    */
   public async secureProtectionSend(
     credential: string,
-    data: any[][]
+    values: any[][]
   ): Promise<{ tokens: string[][] }> {
     const { publicKey: clientPublicKey, secretKey: clientPrivateKey } =
       nacl.box.keyPair();
@@ -86,9 +76,9 @@ export class Client {
         client_public_key: encodeBase64(clientPublicKey),
       }
     );
-    const fulltexts = data.map((values) =>
-      values.map((value) => {
-        const plaintext = decodeUTF8(String(value));
+    const fulltexts = values.map((value) =>
+      value.map((v) => {
+        const plaintext = decodeUTF8(JSON.stringify(v));
         const nonce = nacl.randomBytes(nacl.box.nonceLength);
         const serverPublicKey = decodeBase64(server_public_key);
         const ciphertext = nacl.box(
@@ -105,9 +95,158 @@ export class Client {
     );
     const { tokens } = await this.#request(
       "POST",
-      new URL("/api/secure/protection/insert", this.#kastelaUrl),
-      { credential, data: fulltexts }
+      new URL("/api/secure/protection/store", this.#kastelaUrl),
+      { credential, values: fulltexts }
     );
     return { tokens };
+  }
+
+  /** Receive encrypted protection data from server.
+   * @param {string} credential secure protection credential
+   * @param {string[][]} tokens token to be sent to the server. the sequence of token match the protection_id sequence on init
+   * @return {Promise<{values: any[][]}>} token that will be stored in the database. the tokens sequence corresponds to the data sequence
+   * @example
+   * 	// send "123","456","789" to server
+   * client.secureProtectionReceive("yourCredential", [["foo","bar"],["baz"]])
+   */
+  public async secureProtectionReceive(
+    credential: string,
+    tokens: string[][]
+  ): Promise<{ values: any[][] }> {
+    const { publicKey: clientPublicKey, secretKey: clientPrivateKey } =
+      nacl.box.keyPair();
+    const { server_public_key } = await this.#request(
+      "POST",
+      new URL("/api/secure/protection/begin", this.#kastelaUrl),
+      {
+        credential,
+        client_public_key: encodeBase64(clientPublicKey),
+      }
+    );
+    const { values: fulltexts } = await this.#request(
+      "POST",
+      new URL("/api/secure/protection/fetch", this.#kastelaUrl),
+      {
+        credential,
+        tokens,
+      }
+    );
+    const values = fulltexts.map((v: any[]) =>
+      v.map((w) => {
+        const fulltext = decodeBase64(w);
+        const nonce = fulltext.subarray(0, nacl.box.nonceLength);
+        const ciphertext = fulltext.subarray(nacl.box.nonceLength);
+        const serverPublicKey = decodeBase64(server_public_key);
+        const plaintext = nacl.box.open(
+          ciphertext,
+          nonce,
+          serverPublicKey,
+          clientPrivateKey
+        );
+        if (!plaintext) {
+          throw new Error("decryption failed");
+        }
+        return JSON.parse(encodeUTF8(plaintext));
+      })
+    );
+    return { values };
+  }
+
+  /** Send encrypted vault data to server.
+   * @param {string} credential secure vault credential
+   * @param {any[][]} values vault data to be sent to the server. the sequence of data match the vault_id sequence on init
+   * @return {Promise<{tokens: string[][]}>} token that will be stored in the database. the tokens sequence corresponds to the data sequence
+   * @example
+   * 	// send "123","456","789" to server
+   * client.securevaultSend("yourCredential", [["123","456"],["789"]])
+   */
+  public async secureVaultSend(
+    credential: string,
+    values: any[][]
+  ): Promise<{ tokens: string[][] }> {
+    const { publicKey: clientPublicKey, secretKey: clientPrivateKey } =
+      nacl.box.keyPair();
+    const { server_public_key } = await this.#request(
+      "POST",
+      new URL("/api/secure/vault/begin", this.#kastelaUrl),
+      {
+        credential,
+        client_public_key: encodeBase64(clientPublicKey),
+      }
+    );
+    const fulltexts = values.map((value) =>
+      value.map((v) => {
+        const plaintext = decodeUTF8(JSON.stringify(v));
+        const nonce = nacl.randomBytes(nacl.box.nonceLength);
+        const serverPublicKey = decodeBase64(server_public_key);
+        const ciphertext = nacl.box(
+          plaintext,
+          nonce,
+          serverPublicKey,
+          clientPrivateKey
+        );
+        const fulltext = new Uint8Array(nonce.length + ciphertext.length);
+        fulltext.set(nonce);
+        fulltext.set(ciphertext, nonce.length);
+        return encodeBase64(fulltext);
+      })
+    );
+    const { tokens } = await this.#request(
+      "POST",
+      new URL("/api/secure/vault/store", this.#kastelaUrl),
+      { credential, values: fulltexts }
+    );
+    return { tokens };
+  }
+
+  /** Receive encrypted vault data from server.
+   * @param {string} credential secure vault credential
+   * @param {string[][]} tokens token to be sent to the server. the sequence of token match the vault_id sequence on init
+   * @return {Promise<{values: any[][]}>} token that will be stored in the database. the tokens sequence corresponds to the data sequence
+   * @example
+   * 	// send "123","456","789" to server
+   * client.secureVaultReceive("yourCredential", [["foo","bar"],["baz"]])
+   */
+  public async secureVaultReceive(
+    credential: string,
+    tokens: string[][]
+  ): Promise<{ values: any[][] }> {
+    const { publicKey: clientPublicKey, secretKey: clientPrivateKey } =
+      nacl.box.keyPair();
+    const { server_public_key } = await this.#request(
+      "POST",
+      new URL("/api/secure/vault/begin", this.#kastelaUrl),
+      {
+        credential,
+        client_public_key: encodeBase64(clientPublicKey),
+      }
+    );
+    const { values: fulltexts } = await this.#request(
+      "POST",
+      new URL("/api/secure/vault/fetch", this.#kastelaUrl),
+      {
+        credential,
+        tokens,
+      }
+    );
+    const values = fulltexts.map((v: any[]) =>
+      v.map((w) => {
+        const fulltext = decodeBase64(w);
+        const nonce = fulltext.subarray(0, nacl.box.nonceLength);
+        const ciphertext = fulltext.subarray(nacl.box.nonceLength);
+        const serverPublicKey = decodeBase64(server_public_key);
+        const plaintext = nacl.box.open(
+          ciphertext,
+          nonce,
+          serverPublicKey,
+          clientPrivateKey
+        );
+        if (!plaintext) {
+          throw new Error("decryption failed");
+        }
+        return JSON.parse(encodeUTF8(plaintext));
+      })
+    );
+    return { values };
   }
 }
